a39cdf876248c3690abaf599ba5e9c7d621bb2ed
jcguerra10 <47890788+jcguerra10@users.noreply.github.com>
2024-10-25 09:23:45 -0500

    Update DataSDLocal.ino

--- DataSDLocal.ino
+++ DataSDLocal.ino
@@ -1,40 +1,43 @@
 // -------------------------- Libraries ----------------------------
 #include <DHT.h>
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
 #include <SPI.h>
-#include <SD.h>
-
+#include <SdFat.h>
 #include <Wire.h>
 #include <DS3231.h>
 #include <Adafruit_VEML7700.h>
+
 // -------------------------- Definitions --------------------------
 // -------------------- DHT11 --------------------
-#define PIN_DHT11 7
+#define PIN_DHT11 4
 #define DHTTYPE DHT11
-// -------------------- DS18B20 ------------------
-// #define ONE_WIRE_BUS 6
 // -------------------- AM312 --------------------
 #define timeSeconds 2
 
-// --------------------------------- Set-up -------------------------------
-// -------------------- DHT11 --------------------
-DHT dht(PIN_DHT11, DHTTYPE);
-// -------------------- DS18B20 ------------------
-// // Setup a oneWire instance to communicate with any OneWire devices
-// OneWire oneWire(ONE_WIRE_BUS);
-// // Pass our oneWire reference to Dallas Temperature sensor 
-// DallasTemperature sensors(&oneWire);
-// -------------------- AM312 --------------------
-// Set GPIOs for LED and PIR Motion Sensor
-const int led = LED_BUILTIN;
-const int motionSensor = 5;
+// -------------------- SdFat -----------------------
+SdFat SD;       // Usa SdFat en lugar de SD
+File dataFile;  // Define el archivo para almacenar datos
+
+const int chipSelect = SS1;  // Define el pin de selección del chip (ajústalo según tu placa)
+
 // -------------------- VEML7700 -----------------
 Adafruit_VEML7700 veml = Adafruit_VEML7700();
-// -------------------- SD -----------------------
-const int chipSelect = SDCARD_SS_PIN;
+
+// -------------------- DHT11 --------------------
+DHT dht(PIN_DHT11, DHTTYPE);
+
+// -------------------- Thermistor ---------------
+float current;
+int termPin = 0;          // The analog pin to which the thermistor is connected is A0
+int termNom = 100000;     // Thermistor reference resistance
+int refTemp = 28;         // Temperature for reference resistance
+int beta = 3950;          // Beta factor
+int resistance = 100000;  // value of resistance in series in the circuit
+
+// -------------------- AM312 --------------------
+const int led = LED_BUILTIN;
+const int motionSensor = 5;
 
 // Timer: Auxiliary variables
 unsigned long now = millis();
@@ -49,64 +52,46 @@ void detectsMovement() {
   lastTrigger = millis();
 }
 
-//
+// RTC
 RTClib myRTC;
 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
 
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
   // -------------------- VEML7700 -----------------
   if (!veml.begin()) {
     Serial.println("Sensor not found");
-    while (1);
+    while (1)
+      ;
   }
 
   veml.setGain(VEML7700_GAIN_1_8);
   veml.setIntegrationTime(VEML7700_IT_100MS);
 
-  // -------------------- SD -----------------------
-  // Serial.print("Initializing SD card...");
+  // -------------------- SdFat -----------------------
+  Serial.print("Initializing SD card with SdFat...");
 
-  // // see if the card is present and can be initialized:
-  // if (!SD.begin(chipSelect)) {
-  //   Serial.println("Card failed, or not present");
-  //   // don't do anything more:
-  //   while (1);
-  // }
-  // Serial.println("card initialized.");
-  // -------------------- GENERAL -------------------
+  // Intenta inicializar la tarjeta SD
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    while (1)
+      ;  // Si falla, detén la ejecución
+  }
+  Serial.println("Card initialized.");
 
   dht.begin();
-  // sensors.begin();
 
   // -------------------- AM312 --------------------
   pinMode(motionSensor, INPUT_PULLUP);
-  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
   attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
 
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
-
-  Wire.begin();
-
 }
 
-// --------------------------------- Loop ---------------------------------
 void loop() {
   delay(2000);
-
-  // -------------------- SD -----------------------
-  // make a string for assembling the data to log:
-  String dataString = "";
-  // -------------------- SD -----------------------
-
-
-  // Obtener el tiempo de captura en milisegundos
-  unsigned long timeMillis = millis();
 
   // -------------------- DHT11 --------------------
   float h = dht.readHumidity();
@@ -117,99 +102,78 @@ void loop() {
   }
   float hic = dht.computeHeatIndex(t, h, false);
 
-  // -------------------- DS18B20 ------------------
-  // sensors.requestTemperatures();
+  DateTime now = myRTC.now();
+
+  // ---
+  current = analogRead(termPin);
+  current = 1023 / current - 1;
+  current = resistance / current;
+
+  float temperature = current / termNom;    // (R/Ro)
+  temperature = log(temperature);           // ln(R/Ro)
+  temperature /= beta;                      // 1/B * ln(R/Ro)
+  temperature += 1.0 / (refTemp + 273.15);  // + (1/To)
+  temperature = 1.0 / temperature;          // The inverted value
+  temperature -= 273.15;                    // Convert from Kelvin to Celsius
+
+  unsigned int tempRealHex = (unsigned int)(temperature * 100);  // Multiplicar por 100 para enviar un valor entero
+  // ---
 
   // -------------------- AM312 --------------------
-  if((digitalRead(led) == HIGH) && (motion == false)) {
+  if ((digitalRead(led) == HIGH) && (motion == false)) {
     motion = true;
   }
 
-  DateTime now = myRTC.now();
+  // -------------------- VEML7700 -----------------
+  float lux = veml.readLux();
 
   // -------- PRINT / SAVE --------
-  Serial.println("-----------------------------------------------");
-  Serial.print("Capture Time: ");
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(' ');
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-  Serial.println("-------------------- DHT11 --------------------");
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.println("%");
-  Serial.print(F("Temperature: "));
-  Serial.print(t);
-  Serial.println(F("°C "));
-  Serial.print(F("°Heat index: "));
-  Serial.print(hic);
-  Serial.println(F("°C"));
-
-  // Serial.println("-------------------- DS18B20 ------------------");
-  // Serial.print("Celsius temperature: ");
-  // Serial.println(sensors.getTempCByIndex(0)); 
-
-  Serial.println("-------------------- AM312 --------------------");
-  Serial.print("Movement?: ");
-  Serial.println(motion);
-  
-  Serial.println("-------------------- VEML7700 -----------------");
-    Serial.print("lux: "); Serial.println(veml.readLux());
-
-
-  // // -------- PRINT / SAVE --------
+  String dataString = "";
+  // Agregamos los datos del RTC (fecha y hora)
   dataString += String(now.year(), DEC);
-  dataString += String(";");
+  dataString += String("/");
   dataString += String(now.month(), DEC);
-  dataString += String(";");
+  dataString += String("/");
   dataString += String(now.day(), DEC);
-  dataString += String(";");
+  dataString += String(" ");
   dataString += String(now.hour(), DEC);
-  dataString += String(";");
+  dataString += String(":");
   dataString += String(now.minute(), DEC);
-  dataString += String(";");
+  dataString += String(":");
   dataString += String(now.second(), DEC);
-  dataString += String(";");
-  dataString += String(h);
-  dataString += String(";");
-  dataString += String(t);
-  dataString += String(";");
-  // -------------------- DS18B20 ------------------
-  // dataString += String(sensors.getTempCByIndex(0));
-  // dataString += String(";"); 
-  // -------------------- AM312 --------------------
-  dataString += String(motion);
-  dataString += String(";");
-  dataString += String(veml.readLux());
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one befores opening another.
-  File dataFile = SD.open("data.csv", FILE_WRITE);
+  // Agregamos los datos de los sensores al dataString
+  dataString += String("; DHT Humedad: ");
+  dataString += String(h);  // Humedad del DHT11
+  dataString += String(" %");
 
-  // if the file is available, write to it:
+  dataString += String("; DHT Temperatura: ");
+  dataString += String(t);  // Temperatura del DHT11
+  dataString += String(" °C");
+
+  dataString += String("; Thermistor: ");
+  dataString += String(temperature);  // Índice de calor calculado
+  dataString += String(" °C");
+
+  dataString += String("; Movimiento detectado: ");
+  dataString += String(motion ? "Sí" : "No");  // Estado del sensor de movimiento
+
+  dataString += String("; Luz (lux): ");
+  dataString += String(lux);  // Lectura del sensor VEML7700
+
+  // -------------------- SdFat -----------------------
+  // Abre el archivo para escribir
+  dataFile = SD.open("data.csv", FILE_WRITE);
+
   if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    // print to the serial port too:
-    Serial.println(dataString);
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening data.csv");
-    dataFile.close();
+    dataFile.println(dataString);  // Escribe en el archivo
+    dataFile.close();              // Cierra el archivo
+    Serial.println(dataString);    // Imprime también en el monitor serial
+  } else {
+    Serial.println("Error opening data.csv");
   }
 
   digitalWrite(led, LOW);
   startTimer = false;
   motion = false;
-
-
 }
